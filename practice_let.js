6f748a419d827e58472ebb4d1a884add9c201147
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-05-19 15:10:43 +0900

    Javascript10

---
+++ practice_let.js
@@ -0,0 +1,11 @@
+const price = 150;
+const rate = 1.1;
+
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
+
+price = 151;
+console.log(price * 140 * rate);
+console.log(price * 140 * rate);
+console.log(price * 140 * rate);
