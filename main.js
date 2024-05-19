63b4601e25d7aebbd06ffc1fb8d72b3bb710ecfe
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-05-19 15:37:04 +0900

    #11 文字列を表現してみよう

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 'use strict';
 
-console.log('It is a pen.');
-console.log('It\'s  a pen.');
-// 改行したいときはバックスラッシュプラスnを使う
-console.log('It\'s  \na \npen.');
+// My name is Taro Yamada, call me Taro!
+
+const first_name = 'Taro';
+const last_name = 'Yamada';
+
+// 定数や変数は’’を使わないで表示させる
+// console.log('My name is' + first_name + '' + last_name + 'call me' + first_name);
+// console.log('My name is ' + first_name + ' ' + last_name + ', call me ' + first_name);
+
+console.log('My name is ${first_name} ${last_name}, call me ${first_name}');
+console.log(`My name is ${first_name} ${last_name}, call me ${first_name}`);
