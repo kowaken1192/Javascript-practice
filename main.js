561199f498ddbd92ecd7d71a3a01081544550f94
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-05-19 16:43:50 +0900

    #20 forの中でforを使ってみよう (02:27)

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 'use strict';
 
-for (let i = 0; i < 3; i++) {
-    // console.log('Hello');
-    console.log(`&{i}: Hello`);
+const rate = 1.1;
+
+for (let price = 150; price <= 160; price++) {
+    console.log(`Price: ${price}`)
+    for (let amount = 120; amount <= 140; amount+=10) {
+        console.log(`Amount: ${amount}`)
+        console.log(price * amount * rate);
+    }  
 }
