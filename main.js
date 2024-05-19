d8ecdba8a5b0234cad73e3a2f194c63eeb361034
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-05-19 16:03:58 +0900

    #15 if ... else ... で条件分岐をしてみよう

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// ひとつが文字列になると連結される
-console.log(50 + '20');
+const score = Number(prompt('Score?'));
 
-// promptメソッドを使うとユーザーからのの入力を受けとる
-// 加えて必ず文字列を返す仕様になる
-// 変更するには必ずNumberを使う
-const n = prompt('Any number?');
-console.log(Number(n) + 10);
+if (score >= 90) {
+    console.log('合格です');
+} else {
+    console.log('アホだな残念！');
+}
