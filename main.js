f1e7019e90c30b298b73a5f74d6dc31845f2bcd7
小若憲史 <kowakakenji@kowakaknjinoMBP.lan>
2024-05-19 15:21:19 +0900

    Javascript11

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 'use strict';
 
-let price;
-const rate = 1.1;
-
-price = 150;
-console.log(price * 120 * rate); // 150 * 120 * 1.1
-console.log(price * 130 * rate); // 150 * 130 * 1.1
-console.log(price * 140 * rate); // 150 * 140 * 1.1
-
-// price = 151;
-// price += 1;
-// price *= 1.5;
-
-console.log(price * 140 * rate); // 151 * 140 * 1.1
-console.log(price * 140 * rate); // 151 * 140 * 1.1
-console.log(price * 140 * rate); // 151 * 140 * 1.1
-
+console.log('It is a pen.');
+console.log('It\'s  a pen.');
+// 改行したいときはバックスラッシュプラスnを使う
+console.log('It\'s  \na \npen.');
